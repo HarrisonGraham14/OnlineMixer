544edf9acd89c9ff24aa1e5905444f8e9aab5e38
BuildTools <unconfigured@null.spigotmc.org>
2024-07-29 19:52:12 +1000

    Added audio context and basic mixing functionality with sample tracks

--- script.js
+++ script.js
@@ -1,12 +1,87 @@
-const CHANNELLABELS = ["CH 01", "CH 02", "CH 03", "CH 04", "CH 05", "CH 06", "CH 07", "CH 08", "CH 09", "CH 10", "CH 11", "CH 12", "CH 13", "CH 14", "CH 15", "CH 16", "USB", "Rtn 1", "Rtn 2", "Rtn 3", "Rtn 4", "Bus 1", "Bus 2", "Bus 3", "Bus 4", "FXSnd 1", "FXSnd 2", "FXSnd 3", "FXSnd 4", "LR", "DCA 1", "DCA 2", "DCA 3", "DCA 4"]
-let layers = [["CH 1-8", [0, 1, 2, 3, 4, 5, 6, 7]], ["CH 9-16", [8, 9, 10, 11, 12, 13, 14, 15]], ["Aux / FX", [16, 17, 18, 19, 20]], ["Bus", [21, 22, 23, 24]], ["FXSnd / Main", [25, 26, 27, 28, 29]], ["DCA", [30, 31,32, 33]]];
-let maxChannels = 8;
+//==================================================================================================================================================================================================================================================================
+//                                                                                                                          AUDIO CONTEXT
+//==================================================================================================================================================================================================================================================================
 
-let channelsPanel;
-let blankChannel;
+const audioContext = new AudioContext();
+let audios = [];
+let sources = [];
+let gains = [];
 
-let layersPanel;
-let blankLayer;
+const playButton = document.querySelector(".play-button");
+const pauseButton = document.querySelector(".pause-button");
+const stopButton = document.querySelector(".stop-button");
+const playerBar = document.querySelector(".player-bar");
+const playerTime = document.querySelector(".player-time");
+
+for (let i = 0; i < 16; i++) {
+    audios.push(new Audio());
+    sources.push(audioContext.createMediaElementSource(audios[i]));
+    gains.push(new GainNode(audioContext));
+
+    sources[i].connect(gains[i]);
+    gains[i].connect(audioContext.destination);
+}
+
+function loadAudio(folder, trackCount) {
+    unloadedTracks = trackCount;
+    for (let i = 0; i < trackCount; i++) {
+        let trackPath = folder + "/Track " + (i < 9 ? "0" : "") + (i + 1) + ".wav";
+        audios[i].src = trackPath;
+        allChannels[i].volume = 0;
+    }
+    loadLayer(0);
+}
+
+function play() {
+    if (audioContext.state === "suspended") audioContext.resume();
+    for (let i in audios) {
+        audios[i].play();
+        audios[i].currentTime = audios[0].currentTime;
+    }
+}
+
+function pause() {
+    for (let i in audios) {
+        audios[i].pause();
+    }
+}
+
+function stop() {
+    for (let i in audios) {
+        audios[i].pause();
+        audios[i].currentTime = 0;
+    }
+}
+
+playButton.addEventListener("click", () => play());
+pauseButton.addEventListener("click", () => pause());
+stopButton.addEventListener("click", () => stop());
+
+playerBar.addEventListener("input", (event) => {
+    audioContext.suspend();
+    for (let i in audios) {
+        audios[i].currentTime = playerBar.value;
+    }
+});
+playerBar.addEventListener("change", (event) => audioContext.resume());
+
+function updatePlayerTime() {
+    let currentTime = audios[0].currentTime;
+    let minutes = Math.floor(currentTime / 60);
+    let seconds = Math.floor(currentTime % 60);
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    playerTime.innerHTML = minutes + ":" + seconds; 
+    if (audioContext.state != "suspended") {
+        playerBar.value = currentTime;
+        playerBar.max = audios[0].duration;
+    }
+}
+var intervalId = setInterval(updatePlayerTime, 100);
+
+
+//==================================================================================================================================================================================================================================================================
+//                                                                                                                         CHANNELS/PANELS
+//==================================================================================================================================================================================================================================================================
 
 class Channel {
     label;
@@ -19,26 +94,27 @@ class Channel {
     }
 }
 
+const CHANNELLABELS = ["CH 01", "CH 02", "CH 03", "CH 04", "CH 05", "CH 06", "CH 07", "CH 08", "CH 09", "CH 10", "CH 11", "CH 12", "CH 13", "CH 14", "CH 15", "CH 16", "USB", "Rtn 1", "Rtn 2", "Rtn 3", "Rtn 4", "Bus 1", "Bus 2", "Bus 3", "Bus 4", "FXSnd 1", "FXSnd 2", "FXSnd 3", "FXSnd 4", "LR", "DCA 1", "DCA 2", "DCA 3", "DCA 4"]
+let layers = [["CH 1-8", [0, 1, 2, 3, 4, 5, 6, 7]], ["CH 9-16", [8, 9, 10, 11, 12, 13, 14, 15]], ["Aux / FX", [16, 17, 18, 19, 20]], ["Bus", [21, 22, 23, 24]], ["FXSnd / Main", [25, 26, 27, 28, 29]], ["DCA", [30, 31,32, 33]]];
+let maxChannels = 8;
+
 let allChannels = [];
-
-function onLoad() {
-    for (i in CHANNELLABELS) {
-        allChannels[i] = new Channel(i);
-    }
-
-    channelsPanel = document.getElementsByClassName("channels")[0];
-    blankChannel = document.getElementsByClassName("channel")[0].cloneNode(true);
-
-    layersPanel = document.getElementsByClassName("buttons-right")[0];
-    blankLayer = document.getElementsByClassName("buttons-layer")[0].cloneNode(true);
-
-    refreshLayers();
-    loadLayer(0);
+for (let i in CHANNELLABELS) {
+    allChannels[i] = new Channel(i);
 }
+
+let channelsPanel = document.getElementsByClassName("channels")[0];
+let blankChannel = document.getElementsByClassName("channel")[0].cloneNode(true);
+
+let layersPanel = document.getElementsByClassName("buttons-right")[0];
+let blankLayer = document.getElementsByClassName("buttons-layer")[0].cloneNode(true);
+
+refreshLayers();
+loadLayer(0);
 
 function refreshLayers() {
     layersPanel.innerHTML = "";
-    for (i in layers) {
+    for (let i in layers) {
         let newLayer = blankLayer.cloneNode(true);
         newLayer.id = "layer_" + i;
         newLayer.innerHTML = "<p>" + layers[i][0] + "</p>";
@@ -91,7 +167,7 @@ function loadLayer(layerNum) {
 
     // highlighting layer button on right
     let buttons = layersPanel.getElementsByClassName("buttons-layer");
-    for (i in buttons) {
+    for (let i in buttons) {
         if (buttons[i].tagName != "DIV") continue;
         let highlight = buttons[i].id == "layer_" + layerNum;
         buttons[i].style.color = highlight ? "#39B7B6" : "#B9BABC";
@@ -129,8 +205,9 @@ function updateFader() {
     if (volume < -50) volume = volume * 2 + 50;
     if (volume <= -90) volume = -Infinity;
 
+    gains[channelIndex].gain.value = Math.pow(2, volume/6);
+
     allChannels[channelIndex].volume = volume;
-    console.log(volume);
     this.parentNode.getElementsByClassName("channel-fader-level")[0].innerHTML = faderReadout(volume);
 }
 
@@ -138,3 +215,8 @@ function layerPressed() {
     layerIndex = this.id.substring(6);
     loadLayer(layerIndex);
 }
+
+
+
+
+loadAudio("./audio", 7);
